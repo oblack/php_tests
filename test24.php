5aebd2d6d3fe1b65523ce3537977b2d2a0d0fbbb
Orlin Genchev <ogenchev@gmail.com>
2020-01-07 17:53:47 +0200

    namespace, reference variables

---
+++ test24.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MyProject;
+
+echo __NAMESPACE__; // outputs "MyProject"
+
+//function array_me(array &$param) {
+//    $param = 1;
+//}
+//$var = [];
+//array_me($var);
+//var_dump($var);
+//array_me($var);
