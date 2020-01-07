85f8b6b6a52defaf53c098ac9b67e2170c283f24
Orlin Genchev <ogenchev@gmail.com>
2020-01-07 11:17:56 +0200

    closure

--- test22.php
+++ test22.php
@@ -6,6 +6,20 @@ $filter_even = function ($item) {
     return ($item % 2) == 0;
 };
 
-$output = array_filter($input, $filter_even);
+$output2 = array_filter($input, function ($item) {
+    return ($item % 2) == 0;
+});
 
-print_r($output);
+//print_r($output);
+
+
+function criteria_greater_than($min)
+{
+    return function($item) use ($min) {
+        return $item > $min;
+    };
+}
+
+$output2 = array_filter($input, criteria_greater_than(3));
+
+print_r($output2); // items > 3
